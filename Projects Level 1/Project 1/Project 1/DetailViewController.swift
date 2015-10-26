a471231f32a70779d1fd0a9ea9180f7d3493072e
fernandolicon <wisimata@gmail.com>
2015-10-26 15:34:24 -0700

    Added image view to detail view.

--- Projects Level 1/Project 1/Project 1/DetailViewController.swift
+++ Projects Level 1/Project 1/Project 1/DetailViewController.swift
@@ -9,37 +9,38 @@
 import UIKit
 
 class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    var detailItem: AnyObject? {
+    
+    @IBOutlet weak var detailImageView: UIImageView!
+    
+    
+    var detailItem: String? {
         didSet {
             // Update the view.
             self.configureView()
         }
     }
-
+    
     func configureView() {
         // Update the user interface for the detail item.
-        if let detail = self.detailItem {
-            if let label = self.detailDescriptionLabel {
-                label.text = detail.description
+        if let imageName = detailItem{
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: imageName)
             }
         }
+        
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 

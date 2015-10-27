3eb7bebd7254640a368b97f3c0ee506768fdc0b2
fernandolicon <wisimata@gmail.com>
2015-10-27 15:02:21 -0700

    Added countries flags resources and basic functionality to main view.

--- Projects Level 1/Project2/Project2/ViewController.swift
+++ Projects Level 1/Project2/Project2/ViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    //Game vars
+    var countries = [String]()
+    var score = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        setUpLayout()
+        createCountriesArray()
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjectsInArray(countries) as! [String]
+        askQuestion()
+    }
+    
+    //Setup methods
+    func setUpLayout(){
+        button1.layer.borderWidth = 1.0
+        button1.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button2.layer.borderWidth = 1.0
+        button2.layer.borderColor = UIColor.lightGrayColor().CGColor
+        button3.layer.borderWidth = 1.0
+        button3.layer.borderColor = UIColor.lightGrayColor().CGColor
+    }
+    
+    func createCountriesArray(){
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+    }
+    
+    //Game methods
+    func askQuestion(){
+        button1.setImage(UIImage(named: countries[0]), forState: .Normal)
+        button2.setImage(UIImage(named: countries[1]), forState: .Normal)
+        button3.setImage(UIImage(named: countries[2]), forState: .Normal)
     }
 
     override func didReceiveMemoryWarning() {

74b25e3010c0a5b1edeb91a5ac854fe585f637b5
fernandolicon <wisimata@gmail.com>
2015-11-03 09:10:24 -0700

    Added the option to change page by the user.

--- Projects Level 1/Project4/Project4/ViewController.swift
+++ Projects Level 1/Project4/Project4/ViewController.swift
@@ -22,6 +22,11 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        //Navigationbar configuration
+        let barButton = UIBarButtonItem(title: "Open", style: .Plain, target: self, action: "openTapped")
+        navigationItem.rightBarButtonItem = barButton
+        
+        //WK Configuration
         let url = NSURL(string: "https://www.hackingwithswift.com")!
         let urlRequest = NSURLRequest(URL: url)
         webView.loadRequest(urlRequest)
@@ -32,7 +37,31 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK WKNavigation Delegate methods
+    
+    func webView(webView: WKWebView, didFinishNavigation navigation: WKNavigation!) {
+        title = webView.title
+    }
+    
+    //MARK: Action methods
+    
+    func openTapped(){
+        let alertController = UIAlertController(title: "Open page...", message: nil, preferredStyle: .ActionSheet)
+        var alertAction = UIAlertAction(title: "apple.com", style: .Default, handler: openPage)
+        alertController.addAction(alertAction)
+        alertAction = UIAlertAction(title: "hackingwithswift.com", style: .Default, handler: openPage)
+        alertController.addAction(alertAction)
+        alertAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(alertAction)
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func openPage(action: UIAlertAction!){
+        let url = NSURL(string: "https://" + action.title!)!
+        let urlRequest = NSURLRequest(URL: url)
+        webView.loadRequest(urlRequest)
+    }
 
 }
 
